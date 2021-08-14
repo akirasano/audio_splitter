bfd9071e8f425b9ee3347be5f0cd91581c1dace4
sano <akirasn17@gmail.com>
2021-08-15 01:49:49 +0900

    チャプタつけるところまで

---
+++ add_chapter.py
@@ -0,0 +1,89 @@
+import os
+import numpy as np
+import subprocess
+from tqdm import tqdm
+from pydub import AudioSegment
+from split import split_to_mono_numpy
+from matcher import calc_correlation_pyramid
+
+
+def ms_to_timestr(ms):
+    s = ms // 1000
+    min = s // 60
+    h = min // 60
+    min = min % 60
+    s = s % 60
+
+    return f'{h:02d}:{min:02d}:{s:02d}'
+
+
+def write_meta(out, marker_times_ms, end_time_ms):
+    with open(out, 'w') as fp:
+        fp.write(';FFMETADATA\n\n')
+
+        for i in range(len(marker_times_ms)):
+            t0 = marker_times_ms[i]
+            if i == len(marker_times_ms) - 1:
+                t1 = end_time_ms
+            else:
+                t1 = marker_times_ms[i + 1]
+            ts0 = ms_to_timestr(t0)
+            fp.write(
+                '[CHAPTER]\n'
+                'TIMEBASE=1/1000\n'
+                f'START={t0:d}\n'
+                f'END={t1:d}\n'
+                f'title=Chapter{i:d}: {ts0}\n'
+                '\n'
+            )
+
+
+if __name__ == '__main__':
+
+    src = '20210813_Turn.m4a'
+    dst = os.path.splitext(os.path.basename(src))[0] + '.m4b'
+    meta_file = 'tmp_meta.txt'
+
+    markers = [
+        'Turn_jingle_01.wav',
+        'Turn_jingle_02.wav',
+        'Turn_jingle_03.wav',
+        'Turn_jingle_04.wav',
+    ]
+
+    chapter_indices = []
+
+    audio_data = AudioSegment.from_file(src)
+    wave_lch = split_to_mono_numpy(audio_data)[0]
+
+    level = 1024
+
+    last_sample_pos = 0
+    for m in tqdm(markers):
+        marker_data = AudioSegment.from_file(m)
+        marker_wave_lch = split_to_mono_numpy(marker_data)[0]
+
+        coeff = calc_correlation_pyramid(wave_lch, marker_wave_lch, level)
+        args = np.argsort(coeff)
+        for j in range(args.size):
+            sample_pos = args[j] * level
+            if sample_pos > last_sample_pos:
+                chapter_indices.append(sample_pos)
+                last_sample_pos = sample_pos
+                break
+
+    marker_times_ms = [0] + [
+        int(i / audio_data.frame_rate * 1000)
+        for i in chapter_indices
+    ]
+    write_meta(meta_file, marker_times_ms,
+               len(wave_lch) // audio_data.frame_rate * 1000)
+
+    ret = subprocess.run(
+        ['ffmpeg',
+         '-i', src,
+         '-i', meta_file,
+         '-map_chapters', '1',
+         '-acodec', 'copy',
+         dst]
+    )
--- matcher.py
+++ matcher.py
@@ -1,27 +1,15 @@
-# %%
+from itertools import filterfalse
 from joblib import Parallel, delayed
+from numpy.core.shape_base import stack
 from scipy import interpolate
 import scipy
 import matplotlib.pyplot as plt
 import numpy as np
 import time
 import array
-import numpy as np
-from pydub import AudioSegment
+from pydub import AudioSegment, audio_segment
 import simpleaudio
 import split
-# %%
-data = AudioSegment.from_file('20200531_ですよ.m4a')
-# %%
-ln, rn = split.split_to_mono_numpy(data)
-ss = 3*data.frame_rate
-se = 5*data.frame_rate
-ts = 4*data.frame_rate
-te = int(4.2*data.frame_rate)
-sound = ln[ss:se]
-template = ln[ts:te]
-
-# %%
 
 
 def play_numpy(a: np.ndarray, base: AudioSegment, dur=-1):
@@ -33,8 +21,8 @@ def play_numpy(a: np.ndarray, base: AudioSegment, dur=-1):
     po = simpleaudio.play_buffer(
         array.array(base.array_type, a),
         num_channels=1,
-        bytes_per_sample=data.sample_width,
-        sample_rate=data.frame_rate
+        bytes_per_sample=base.sample_width,
+        sample_rate=base.frame_rate
     )
 
     if dur > 0:
@@ -42,28 +30,6 @@ def play_numpy(a: np.ndarray, base: AudioSegment, dur=-1):
         po.stop()
     else:
         po.wait_done()
-
-
-play_numpy(template, data)
-
-# %%
-
-# %%
-ns = ln.size
-x = np.arange(ns)
-f = interpolate.interp1d(x, ln)
-x1 = x[::100]
-fig, ax = plt.subplots(3, 1, figsize=(8, 8))
-ax[0].plot(x, ln, '.')
-ax[1].plot(x1, f(x1), '.')
-ax[2].plot(x, ln, '.')
-ax[2].plot(x1, f(x1), '.')
-for a in ax.flat:
-    a.set_xlim([3e+4, 3.5e4])
-plt.show()
-plt.close(fig)
-
-# %%
 
 
 def correlation(data: np.ndarray, tmpl: np.ndarray):
@@ -76,63 +42,131 @@ def correlation(data: np.ndarray, tmpl: np.ndarray):
     # padded_tml = np.ndarray((data.size, ))
 
     def calc_coeff(data, tmpl, shift):
-        tmp = data.copy()
-        tmp[shift:shift+tmpl.size] -= tmpl
-        return np.linalg.norm(tmp, ord=2)
-    procs = [delayed(calc_coeff)(data, tmpl, s) for s in range(ns)]
+        # tmp = data.copy()
+        tmp = data[shift:shift+tmpl.size] - tmpl
+        return np.linalg.norm(tmp, ord=1) / tmpl.size
+
+    def calc_cross_correlation(data, tmpl, shift):
+        tmp = data[shift:shift+tmpl.size]
+        return -np.abs(np.sum(tmp * tmpl))
+
+    # procs = [delayed(calc_coeff)(data, tmpl, s) for s in range(ns)]
+    procs = [delayed(calc_cross_correlation)(data, tmpl, s) for s in range(ns)]
+
     # for s in range(ns):
     #     tmp = data.copy()
     #     tmp[s:s+tmpl.size] -= tmpl
     #     # padded_tml[:] = 0
     #     # padded_tml[s:s+tmpl.size] = tmpl
     #     coeff[s] = np.linalg.norm(tmp, ord=2)
-    return Parallel(n_jobs=-1, verbose=10, batch_size=16)(procs)
+    return Parallel(n_jobs=-1, verbose=10, batch_size=1024)(procs)
+    # return Parallel(n_j bs=-1)(procs)
 
 
 def calc_correlation_pyramid(
-    data: np.ndarray,
-    tmpl: np.ndarray,
-    # levels=[16, 8, 4, 2, 1]):
-        l):
+        data: np.ndarray, tmpl: np.ndarray, l: int,
+        sampling: bool = True):
 
-    dx = np.arange(data.size)
-    tx = np.arange(tmpl.size)
-    # for l in levels:
-    sdx = np.arange(0, data.size, l)
-    sdata = scipy.interpolate.interp1d(dx, data)(sdx)
+    if sampling:
+        # dx = np.arange(data.size)
+        # sdx = np.arange(0, data.size, l)
+        # sdata = scipy.interpolate.interp1d(dx, data)(sdx)
+        sdata = data[::l]
+        sdata = sdata - sdata.mean()
 
-    stx = np.arange(0, tmpl.size, l)
-    stmpl = scipy.interpolate.interp1d(tx, tmpl)(stx)
+        # tx = np.arange(tmpl.size)
+        # stx = np.arange(0, tmpl.size, l)
+        # stmpl = scipy.interpolate.interp1d(tx, tmpl)(stx)
+        stmpl = tmpl[::l]
+        stmpl = stmpl - stmpl.mean()
+    else:
+        # binning
+        # lの整数倍。後ろは捨てる
+
+        ld = data.size // l * l
+        sdata = data[:ld].reshape(-1, l).sum(axis=1) / l
+
+        ls = tmpl.size // l * l
+        stmpl = data[:ls].reshape(-1, l).sum(axis=1) / l
 
     return np.array(correlation(sdata, stmpl))
 
 
-fig, ax = plt.subplots(3, 1, figsize=(4, 8))
-xdata = np.arange(ln.size) / data.frame_rate + 3
-ax[0].plot(xdata, ln)
-xtemplate = np.arange(template.size) / data.frame_rate + 4
-ax[1].plot(xtemplate, template)
-# ax[2].plot(correlation(ln, template))
-l = 64
-coeff = calc_correlation_pyramid(ln, template, l)
-xcoeff = np.arange(coeff.size) / data.frame_rate * l
-ax[2].plot(xcoeff, coeff)
-amin = np.argmin(coeff)
-print(amin / data.frame_rate * l)
-fig.tight_layout()
-plt.show()
-plt.close(fig)
+if __name__ == '__main__':
+    dsound = AudioSegment.from_file('20210813_Turn.wav')
+    sound = split.split_to_mono_numpy(dsound)[0]
+    dtemplate = AudioSegment.from_file('Turn_jingle_02.wav')
+    template = split.split_to_mono_numpy(dtemplate)[0]
 
+    from scipy import signal
 
-# %%
-fig, ax = plt.subplots(5, 1, figsize=(8, 12))
-for i, l in enumerate([64, 128, 256, 512, 1024]):
-    coeff = calc_correlation_pyramid(ln, template, l)
-    amin = np.argmin(coeff)
-    print(l, amin / data.frame_rate * l)
-    x = np.arange(coeff.size) / data.frame_rate * l
-    ax[i].plot(x, coeff, label=f'{l}')
-    ax[i].plot([x[amin], x[amin]], ax[i].get_ylim())
-    ax[i].grid()
-    ax[i].legend()
-plt.show(fig)
+    def lpf(wave, th, n=5):
+        b, a = signal.butter(1, th, btype='low')
+        for i in range(0, n):
+            wave = signal.filtfilt(b, a, wave)
+        return wave
+
+    # fsound = lpf(sound, 1.0/16.0)
+    fsound = sound
+    # ftemplate = lpf(template, 1.0/16.0)
+    ftemplate = template
+
+    # play_numpy(template, dtemplate, dur=3)
+
+    # level_exps = [10, 11]
+    # levels = [2**e for e in level_exps]
+    levels = [1024]
+    proc_times = []
+    coeffs = []
+    for i, l in enumerate(levels):
+        s = time.perf_counter()
+        coeff = calc_correlation_pyramid(fsound, ftemplate, l)
+        coeffs.append(coeff)
+        e = time.perf_counter()
+        proc_times.append(e - s)
+
+    cmap = plt.get_cmap('tab10')
+    nr = len(levels) + 2
+    fig, ax = plt.subplots(nr, 1, figsize=(8, nr*2))
+    t = np.arange(sound.size) / dsound.frame_rate
+    ax[0].plot(t, sound, color='gray', alpha=0.5)
+    ax[0].set_xlim((1150, 1200))
+    ly0 = ax[0].get_ylim()
+    for i, l in enumerate(levels):
+        ai = i + 1
+        coeff = coeffs[i]
+        amax = np.argmin(coeff)
+        x = np.arange(coeff.size) / dsound.frame_rate * l
+        ax[ai].plot(x, coeff, label=f'{l}', alpha=0.5)
+
+        args = np.argsort(coeff)
+        for j in range(5):
+            amax = args[j]
+            # if amax > 100:
+            ax[ai].plot([x[amax], x[amax]], ax[ai].get_ylim(),
+                        color=cmap(j), alpha=0.5, label=f'{j} th.')
+
+            def s_to_timestr(s):
+                min = s // 60
+                h = int(min // 60)
+                min = int(min % 60)
+                s = int(s % 60)
+                return f'{h:02d}\:{min:02d}\:{s:02d}'
+            print('play', amax, x[amax], s_to_timestr(x[amax]))
+            # play_numpy(sound[amax*l:], dsound, dur=3)
+            # break
+
+        r0 = i / len(levels)
+        r1 = (i + 1) / len(levels)
+        hy = ly0[1] - ly0[0]
+        ly = [ly0[0] + hy*r0, ly0[0] + hy*r1]
+        ax[0].plot([x[amax], x[amax]], ly,
+                   color=cmap(i), alpha=0.9, label=f'{l}')
+    ax[-1].bar(np.arange(len(proc_times)), proc_times, tick_label=levels)
+    ax[-1].set_xlabel('Level')
+    ax[-1].set_ylabel('Proc. time [s]')
+    for a in ax.flat:
+        a.grid()
+        a.legend(loc='lower left')
+    fig.tight_layout()
+    fig.savefig('match_test.png')
